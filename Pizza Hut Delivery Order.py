985af86f455397767ced95832ceb51adb2b2cb0b
Alfieryanfitra23008 <149127138+Alfieryanfitra23008@users.noreply.github.com>
2023-11-01 09:43:59 +0700

    Add files via upload

---
+++ Pizza Hut Delivery Order.py
@@ -0,0 +1,9 @@
+#print pertama dan input
+print ("Selamat Datang di Pizza Hut")
+print ("Daftar Topping Yang Tersedia : \n1. Frankfurter BBQ \n2. Meat Monsta \n3. Super Supreme \n4. Super Supreme Chicken \n5. Meat Lovers \n6. Chicken Lovers \n7. Cheese Lovers \n8. American Favourite ")
+topping = int(input("Silahkan Pilih Topping Anda (1-8): "))
+print ("Daftar Crust Yang Tersedia : \n1. Pan \n2. StuffedCrust Cheese \n3. StuffedCrust Sausage \n4. Cheesy Bites \n5. Crown Crust")
+crust = int(input("Silahkan Pilih Crust Anda (1-5): "))
+print ("Daftar Ukuran Pizza Yang Tersedia : \n1. Personal \n2. Regular \n3. Large")
+ukuran = int(input("Silahkan Pilih Ukuran Pizza Anda (1-3): "))
+xtracz = input("Apakah Anda ingin Menambahkan Extra Cheese (Y/N)? ")
